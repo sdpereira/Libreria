4d6271be80fa0ab08305efb4cd274f047a4ce279
sdpereira <sdpereira@misena.edu.co>
2020-09-11 08:20:45 -0500

    Corrección en migraciones

--- database/migrations/2020_09_11_030209_create_autors_table.php
+++ database/migrations/2020_09_11_030209_create_autors_table.php
@@ -14,13 +14,11 @@ class CreateAutorsTable extends Migration
     public function up()
     {
         Schema::create('autors', function (Blueprint $table) {
+
             $table->increments('id');
             $table->string('nombre',150);
             $table->string('id_pais',10);
             $table->foreign('id_pais')->references('id')->on('pais');
-
-
-
             $table->timestamps();
         });
     }
@@ -33,5 +31,6 @@ class CreateAutorsTable extends Migration
     public function down()
     {
         Schema::dropIfExists('autors');
+
     }
 }
